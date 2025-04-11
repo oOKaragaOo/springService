da2c8a9972db6ec96ca1428b199f778ba68dd39c
kanhoon <kanhoon2016@gmail.com>
2025-04-11 14:03:54 +0700

    done

--- src/main/java/com/example/springservice/SecurityConfig.java
+++ src/main/java/com/example/springservice/SecurityConfig.java
@@ -3,7 +3,6 @@ package com.example.springservice;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
@@ -12,7 +11,6 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import org.springframework.web.cors.CorsConfigurationSource;
-import org.springframework.web.filter.CorsFilter;
 
 import java.util.List;
 import org.springframework.http.HttpMethod;
